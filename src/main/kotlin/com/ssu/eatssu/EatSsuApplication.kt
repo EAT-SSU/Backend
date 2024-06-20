1e5f50ed8971e3b27978426ff547146ccbbe3fe9
Geonwoo Pack <packdev937@gmail.com>
2024-06-20 23:47:51 +0900

    initial commit

---
+++ src/main/kotlin/com/ssu/eatssu/EatSsuApplication.kt
@@ -0,0 +1,11 @@
+package com.ssu.eatssu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EatSsuKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<EatSsuKotlinApplication>(*args)
+}
