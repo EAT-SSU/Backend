669677902585184673e91cab689a1131191ebeb9
Geonwoo Pack <packdev937@gmail.com>
2024-06-25 11:28:36 +0900

    #3 test : 유저 마이페이지 조회가 가능한지 테스트 한다

---
+++ src/test/kotlin/com/ssu/eatssu/domain/user/application/UserQueryServiceTest.kt
@@ -0,0 +1,30 @@
+package com.ssu.eatssu.domain.user.application
+
+import com.ssu.eatssu.domain.user.entity.User
+import com.ssu.eatssu.domain.user.persistence.UserRepository
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserQueryServiceTest @Autowired constructor(
+    private val userQueryService: UserQueryService,
+    private val userRepository: UserRepository,
+) {
+
+    @Test
+    @DisplayName("유저 마이페이지를 조회한다")
+    fun getUserMypageTest() {
+        // given
+        val user = userRepository.save(User.fixture())
+
+        // when
+        val response = userQueryService.getUserMypage(user.id!!)
+
+        // then
+        assertEquals(user.nickname, response.nickname)
+        assertEquals(user.provider.name, response.provider)
+    }
+}
