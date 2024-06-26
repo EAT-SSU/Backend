4155f4fb4abcbc61503ed7a9c44abedd83b3c2fe
Geonwoo Pack <packdev937@gmail.com>
2024-06-26 17:02:22 +0900

    #5 feat : CQRS 패턴을 준수한 Review 서비스 코드를 작성한다

--- src/main/kotlin/com/ssu/eatssu/global/enums/ErrorMessages.kt
+++ src/main/kotlin/com/ssu/eatssu/global/enums/ErrorMessages.kt
@@ -7,6 +7,8 @@ enum class ErrorMessages(val message: String) {
     NOT_EXIST_TYPE("해당 타입이 존재하지 않습니다."),
     NOT_EXIST_TIMEPART("해당 시간대가 존재하지 않습니다."),
     NOT_EXIST_USER("해당 유저가 존재하지 않습니다."),
+    NOT_EXISTS_MENU("해당 메뉴가 존재하지 않습니다."),
+    NOT_EXISTS_MEAL("해당 식사가 존재하지 않습니다."),
 
     ALREADY_EXIST_NICKNAME("이미 존재하는 닉네임입니다."),
     ALREADY_EXIST_EMAIL("이미 존재하는 이메일입니다."),
