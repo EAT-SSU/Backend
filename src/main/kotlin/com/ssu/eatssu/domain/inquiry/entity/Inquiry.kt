2ac8846fc36c29e442bc5bcf8714204440f508f5
Geonwoo Pack <packdev937@gmail.com>
2024-06-26 16:48:20 +0900

    #3 feat : Inquiry 엔티티를 생성한다

---
+++ src/main/kotlin/com/ssu/eatssu/domain/inquiry/entity/Inquiry.kt
@@ -0,0 +1,46 @@
+package com.ssu.eatssu.domain.inquiry.entity
+
+import com.ssu.eatssu.domain.user.entity.User
+import com.ssu.eatssu.global.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+class Inquiry(
+    @Column(name = "inquiry_title")
+    val title: String,
+
+    @Column(name = "inquiry_content")
+    val content: String,
+
+    @Column(name = "inquiry_email")
+    val email: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    val user: User,
+
+    @Enumerated(EnumType.STRING)
+    val inquiryStatus: InquiryStatus,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inquiry_id")
+    val id: Long? = null,
+) : BaseEntity() {
+    companion object {
+        fun of(
+            user: User,
+            title: String,
+            content: String,
+            email: String
+        ): Inquiry {
+            return Inquiry(
+                title,
+                content,
+                email,
+                user,
+                InquiryStatus.HOLD
+            )
+        }
+    }
+}
---
+++ src/main/kotlin/com/ssu/eatssu/domain/inquiry/entity/InquiryStatus.kt
@@ -0,0 +1,7 @@
+package com.ssu.eatssu.domain.inquiry.entity
+
+enum class InquiryStatus {
+    WAITING,
+    ANSWERED,
+    HOLD
+}
